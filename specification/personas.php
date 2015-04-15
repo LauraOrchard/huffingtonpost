9a0b2ed898b96239037bc9aca2168bf1fe71462e
Laura Orchard <artist@lauraorchard.com>
2015-04-15 11:32:10 -0600

    4/14/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.
    Fleshing out the personas rather than using brief bullet points.
    
    Same as above. Also doing research on actual demographics of math nerds.
    
    Continuing with the details of the personas.
    
    Hashing out the details of the html.

--- specification/personas.php
+++ specification/personas.php
@@ -13,29 +13,37 @@
 				</p>
 			<ul>
 				<li>The male Math Nerd will most likely be between 12 and 35, still living at home while attending high
-					school, college, graduate school, and their doctoral studies in pure math, chemistry, engineering, and physics, with a minor
+					school, college, graduate school, and eventually their doctoral studies in the fields of pure math, chemistry, engineering, and physics, with a minor
 					in classical scoring for piano and oboe. With a close circle of other scientists and musicians, he will
 					want to re-post this piece to all of his friends, challenging them to compete in solving the problem without looking at
-					the proposed solution. The male Math Nerd will be using a mobile device or a desktop that is
+					the proposed solution and rendering their answer in the key of A minor. The male Math Nerd will be using a mobile device or a desktop that
+				shares his gaming equipment.
 				<li>Female Math Nerds, although a smaller segment of the demographic, will most likely be between the ages of 8 and 25, having metriculated from their STEM programs
-				and post-doctoral studies early. Whether she's gathering information for the future, or preparing
-					for her next class agenda, she will forward this piece to herself at her .edu address with a reminder
-					to incorporate the challenge into her curriculum. She'll also use it as an interview question for her
+				and post-doctoral studies early. Whether she's gathering challenge questions to determine who plays first
+					in her 3D rendered Kal-toh game, or preparing for her next class agenda, she will forward this piece to herself at her .edu address with a reminder
+					to use it later. She'll also use it as an interview question to select her
 					interns. The female Math Nerd will be using a tablet exclusively.
 				</li>
 			</ul>
-			<p>The goal for both genders when reading the Huffington Post's "Cheryl's Birthday Math Problem" is to glean
-			the article quickly to determine the level of the challenge, whether they've seen it before, and whether it
-			will be of interest to either their math friends or their students. To be able to read it quickly, comprehend it, and have all of
-			the external and media links work seamlessly will determine whether they re-post or forward it.<br />
+			<p>The goal for both genders when reading the Huffington Post's "Cheryl's Birthday Math Problem" is to quickly glean
+			from the article:
+				<ul>
+					<li>whether they've seen the challenge before,</li>
+					<li>the level of the challenge, and</li>
+					<li>whether it merits a comment from them in the comments section.</li>
+				</ul>
+			If they decide it will be of interest to either their math friends or their students, it is imperative that all of
+			the external links and media links work seamlessly so they can re-post or forward it.<br />
 			<br />
-			A secondary goal will be to prove their intelligence by posting a criticism of the challenge, the writer, the
+			A secondary goal will be to prove their superiority by posting a criticism of the challenge, the writer, the
 			elements of the math problem, the previous commentor, or the color of the sky. The ability to easily comment will
-			effect their willingness to wrestle it out.</p>
+			effect their willingness to wrestle it out with the other nerds.
+			</p>
 		</blockquote>
 		<blockquote>
 
 			<h3>Persona #2: Blogger</h3>
+
 			<ol>
 				<li>Age: between 12 and 55.</li>
 				<li>Goal: to easily locate and access both entertaining and informative articles that can be included as a link from their blog post.</li>
