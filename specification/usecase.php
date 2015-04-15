5181473f58940a5df58286d67ab24355a8d4e8c8
Laura Orchard <artist@lauraorchard.com>
2015-04-15 13:14:43 -0600

    4/14/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.
                                        Fleshing out the personas rather than using brief bullet points.
    
                                        Same as above. Also doing research on actual demographics of math nerds.
    
                                        Continuing with the details of the personas.
    
                                        Hashing out the details of the html.
    
                                        Added the second Persona of "Blogger" and completed the html check. Will review with Rochelle now.
    
                                        Did not review with Rochelle yet, but began the Usecases file in html.
    
                                        Completed Usecases for review with Dylan or Rochelle.

--- specification/usecase.php
+++ specification/usecase.php
@@ -8,44 +8,52 @@
 	<body>
 		<h3>Math Nerd</h3>
 		<blockquote>
-			<p>
-				<ul>
-					<li>How? From a moble phone, a Male will click through from a Tweet sent from a follower. From a tablet, a Female will open the article while
-					browsing Huffington Post.</li>
-					<li>When? While riding the Amtrak from NYC to DC.</li>
-					<li>Where? In a wifi enabled train car.</li>
-					<li>Purpose? Entertainment.</li>
-					<li>Scenarios? For the male, a challenge is posited by a friend. For the female, she is collecting challenges
-					for future use.</li>
-					<li>Necessary Steps?</li>
-						<ol>
-							<li>Receive Twitter with Huffpost link;</li>
-							<li>Arrive at article page on http://www.huffingtonpost.com/2015/04/15/cheryls-birthday-answer_n_7067790.html;</li>
-							<li>Read article by scrolling down page;</li>
-							<li>Click on embedded media link to watch clip ;</li>
-							<li>Scroll down to Comments section;</li>
-							<li>Scroll down to Comments section to read posted comments;</li>
-							<li>Click "Like" and "Follow" on various comments;</li>
-							<li>Click on Post Comment button;</li>
-							<li>Write comment</li>
-						</ol>
+			<ul>
+				<li>How? From a moble phone, a Male will click through from a Tweet sent from a follower. From a tablet, a Female will open the article while
+				browsing Huffington Post.</li>
+				<li>When? 24/7, while riding the Amtrak from NYC to DC.</li>
+				<li>Where? In a wifi enabled train car.</li>
+				<li>Purpose? Entertainment.</li>
+				<li>Scenarios? For the male, a friendly contest is posited by a Twitter follower. For the female, she is collecting challenges
+				for future use.</li>
+				<li>Necessary Steps?</li>
+					<ol>
+						<li>Receive Twitter with Huffpost link;</li>
+						<li>Arrive at article page on http://www.huffingtonpost.com/2015/04/15/cheryls-birthday-answer_n_7067790.html;</li>
+						<li>Read article by scrolling down page;</li>
+						<li>Click on embedded media link to watch clip ;</li>
+						<li>Scroll down to Comments section;</li>
+						<li>Scroll down to Comments section to read posted comments;</li>
+						<li>Click "Like" and "Follow" on various comments;</li>
+						<li>Facebook login screen opens for user id and password;</li>
+						<li>Click on Post Comment button;</li>
+						<li>Write comment;</li>
+						<li>Click "Post" button.</li>
+					</ol>
 			</ul>
-			</p>
-
 		</blockquote>
 		<h3>Blogger</h3>
 		<blockquote>
-			<p>
 			<ul>
-				<li>How?</li>
-				<li>When?</li>
-				<li>Where?</li>
-				<li>Purpose?</li>
-				<li>Scenarios?</li>
-				<li>Necessary Steps</li>
-			</ul>
-			</p>
+				<li>How? From a tablet using wifi.</li>
+				<li>When? 24/7</li>
+				<li>Where? In an all-night coffee shop with wifi.</li>
+				<li>Purpose? Collecting the writing of others to include in a post.</li>
+				<li>Scenarios? While browsing articles on the Huffington Post home page, will see
+					the "Cheryl's Birthday Math Challenge", and will click on the image to open http://www.huffingtonpost.com/2015/04/15/cheryls-birthday-answer_n_7067790.html</li>
+				<li>Necessary Steps?</li>
+				<ol>
+					<li>Click on image from home page of Huffington Post;</li>
+					<li>Scroll down "Cheryl's Birthday" article;</li>
+					<li>Click on media link to watch embedded clip;</li>
+					<li>Scroll down to Comments section to read posted comments;</li>
+					<li>Click "Like" and "Follow" on various comments;</li>
+					<li>Facebook login screen opens for user id and password;</li>
+					<li>Click on Post Comment button;</li>
+					<li>Click "Post" button.</li>
 
+				</ol>
+			</ul>
 		</blockquote>
 	</body>
 </html>
