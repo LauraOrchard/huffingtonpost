9aa134fbc7996f337426912feacb4330e8a7f626
Laura Orchard <artist@lauraorchard.com>
2015-04-15 12:14:02 -0600

    4/14/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.
                                        Fleshing out the personas rather than using brief bullet points.
    
                                        Same as above. Also doing research on actual demographics of math nerds.
    
                                        Continuing with the details of the personas.
    
                                        Hashing out the details of the html.
    
                                        Added the second Persona of "Blogger" and completed the html check. Will review with Rochelle now.
    
                                        Did not review with Rochelle yet, but began the Usecases file in html.

--- specification/usecase.php
+++ specification/usecase.php
@@ -1,7 +1,51 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lauraorchard
- * Date: 4/15/15
- * Time: 9:36 AM
- */
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>Usecases For Huffington Post</title>
+	</head>
+	<header><h1>Usecases for Huffington Post</h1></header>
+	<body>
+		<h3>Math Nerd</h3>
+		<blockquote>
+			<p>
+				<ul>
+					<li>How? From a moble phone, a Male will click through from a Tweet sent from a follower. From a tablet, a Female will open the article while
+					browsing Huffington Post.</li>
+					<li>When? While riding the Amtrak from NYC to DC.</li>
+					<li>Where? In a wifi enabled train car.</li>
+					<li>Purpose? Entertainment.</li>
+					<li>Scenarios? For the male, a challenge is posited by a friend. For the female, she is collecting challenges
+					for future use.</li>
+					<li>Necessary Steps?</li>
+						<ol>
+							<li>Receive Twitter with Huffpost link;</li>
+							<li>Arrive at article page on http://www.huffingtonpost.com/2015/04/15/cheryls-birthday-answer_n_7067790.html;</li>
+							<li>Read article by scrolling down page;</li>
+							<li>Click on embedded media link to watch clip ;</li>
+							<li>Scroll down to Comments section;</li>
+							<li>Scroll down to Comments section to read posted comments;</li>
+							<li>Click "Like" and "Follow" on various comments;</li>
+							<li>Click on Post Comment button;</li>
+							<li>Write comment</li>
+						</ol>
+			</ul>
+			</p>
+
+		</blockquote>
+		<h3>Blogger</h3>
+		<blockquote>
+			<p>
+			<ul>
+				<li>How?</li>
+				<li>When?</li>
+				<li>Where?</li>
+				<li>Purpose?</li>
+				<li>Scenarios?</li>
+				<li>Necessary Steps</li>
+			</ul>
+			</p>
+
+		</blockquote>
+	</body>
+</html>
