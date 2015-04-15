8e906b354f7a626c14ebc0325c19360de1bf0db6
Laura Orchard <artist@lauraorchard.com>
2015-04-15 10:19:42 -0600

    4/14/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.

---
+++ specification/personas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>Data Design Project - April 14, 2015 - Personas</title>
+	</head>
+	<title>Huffington Post Article Personas</title>
+	<body>
+<h3>Persona #1: Math Nerd</h3>
+		<blockquote>
+			<ol>
+				<li>Age: males between the ages of 12 and 75, and females of any age who have graduated from STEM programs.</li>
+				<li>Goals:
+				<ul>
+					<li>to easily locate and access entertaining recreational articles;</li>
+					<li>to quickly locate and access timely, informative news articles.</li>
+				</ul>
+				<li>Technology: both mobile and desktop.</li>
+			</ol>
+				<h3>Persona #2: Blogger</h3>
+			<ol>
+				<li>Age: between 12 and 55.</li>
+				<li>Goal: to easily locate and access both entertaining and informative articles that can be included as a link from their blog post.</li>
+				<li>Technology: both tablet and desktop.</li>
+			</ol>
+		</blockquote>
+	</body>
+</html>
---
+++ specification/usecase.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lauraorchard
+ * Date: 4/15/15
+ * Time: 9:36 AM
+ */
