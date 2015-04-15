905463e329cf94e9fa53601df08f3675ef477a7f
Laura Orchard <artist@lauraorchard.com>
2015-04-15 10:44:57 -0600

    4/14/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.
    Fleshing out the personas rather than using brief bullet points.

--- specification/personas.php
+++ specification/personas.php
@@ -8,6 +8,18 @@
 	<body>
 <h3>Persona #1: Math Nerd</h3>
 		<blockquote>
+			<p>The Math Nerd will can be either male or female, but the ages between the two genders will probably be
+				different.
+				</p>
+			<ul>
+				<li>The male Math Nerd will most likely be between 12 and 35, still living at home while attending high
+					school, college,
+					graduate school, and their doctoral studies in pure math, chemistry, engineering, and physics, with a minor
+					in classical scoring for piano and oboe. With a close circle of other scientists and musicians, he will
+					want to repost this piece to all of his friends to compete in solving the problem without looking at
+					the proposed solution.
+				<li>Females will most likely be between the ages of 8 and 25, having metriculated from their STEM programs
+				and post-doctoral studies early and</li>
 			<ol>
 				<li>Age: males between the ages of 12 and 75, and females of any age who have graduated from STEM programs.</li>
 				<li>Goals:
@@ -17,7 +29,10 @@
 				</ul>
 				<li>Technology: both mobile and desktop.</li>
 			</ol>
-				<h3>Persona #2: Blogger</h3>
+		</blockquote>
+		<blockquote>
+
+			<h3>Persona #2: Blogger</h3>
 			<ol>
 				<li>Age: between 12 and 55.</li>
 				<li>Goal: to easily locate and access both entertaining and informative articles that can be included as a link from their blog post.</li>
