165c95d5bd700db2d6b023f4dd135dbd0d78d07c
Laura Orchard <artist@lauraorchard.com>
2015-04-16 08:44:24 -0600

    4/15/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.
                                        Fleshing out the personas rather than using brief bullet points.
    
                                        Same as above. Also doing research on actual demographics of math nerds.
    
                                        Continuing with the details of the personas.
    
                                        Hashing out the details of the html.
    
                                        Added the second Persona of "Blogger" and completed the html check. Will review with Rochelle now.
    
                                        Did not review with Rochelle yet, but began the Usecases file in html.
    
                                        Completed Usecases for review with Dylan or Rochelle.
    
                                        Tried to add the ERD as a Figure, but it failed.
    
    4/16/15  Updated the usecase descriptions as per Rochelle suggestions.

--- specification/personas.php
+++ specification/personas.php
@@ -8,51 +8,26 @@
 	<body>
 <h3>Persona #1: Passionate Reader/Commenter</h3>
 		<blockquote>
-			<p>The Passionate Reader/Commenter (PRC) is someone who has been directed to a Huffington Post article because
-				of the subject matter in a particular article. They wouldn't general browse HuffPost, but when directed there
+			<p>The Passionate Reader/Commenter (PRC) is someone who has linked directly to a Huffington Post article because
+				of the subject matter. They wouldn't generally browse HuffPost, but when directed there to a specific article
 				by a group Tweet from a like-minded associate, they will read, re-post, re-Tweet, and comment with fervor. To
-				satisfy this type of reader, HuffPost needs to have all of the social media
+				satisfy this type of reader, HuffPost needs to have all of the social media links and commenting functionality
+				working efficiently.<br />
+				The PRC group includes all genders and spans a large age range, but the numbers soar between 16 and 40 years old.<br />
+				The goals of the PRC group are to a) share interesting/informative articles and b) impress their opinions on others
+				through an interactive conversation in the comments section.<br />
+				The PRC group isn't browsing HuffPost, so their devices would be mobile phones and tablets.
 				</p>
-			<ul>
-				<li>The male Math Nerd will most likely be between 12 and 35, still living at home while attending high
-					school, college, graduate school, and eventually their doctoral studies in the fields of pure math, chemistry, engineering, and physics, with a minor
-					in classical scoring for piano and oboe. With a close circle of other scientists and musicians, he will
-					want to re-post this piece to all of his friends, challenging them to compete in solving the problem without looking at
-					the proposed solution and rendering their answer in the key of A minor. The male Math Nerd will be using a mobile device or a desktop that
-				shares his gaming equipment.
-				<li>Female Math Nerds, although a smaller segment of the demographic, will most likely be between the ages of 8 and 25, having metriculated from their STEM programs
-				and post-doctoral studies early. Whether she's gathering challenge questions to determine who plays first
-					in her nearly complete 3D rendered game of Kal-toh, or preparing for her next class agenda, she will forward this piece to herself at her .edu address with a reminder
-					to use it later. She'll also use it as an interview question during the intern selection process. The female Math Nerd will be using a tablet exclusively.
-				</li>
-			</ul>
-			<p>The goal for both genders when reading the Huffington Post's "Cheryl's Birthday Math Problem" is to quickly glean
-			from the article:
-				<ul>
-					<li>whether they've seen the challenge before,</li>
-					<li>the level of the challenge, and</li>
-					<li>whether it merits a comment from them in the comments section.</li>
-				</ul>
-			If they decide it will be of interest to either their math friends or their students, it is imperative that all of
-			the external links and media links work seamlessly so they can re-post or forward it.<br />
-			<br />
-			A goal that both genders share will be to prove their superiority by posting a criticism of the challenge, the writer, the
-			elements of the math problem, the previous commentor, or the color of the sky. The ability to easily comment will
-			effect their willingness to wrestle it out with the other nerds.
-			</p>
 		</blockquote>
 <h3>Persona #2: Blogger</h3>
 		<blockquote>
-			<p>The Blogger will be either male of female, between the ages of 12 and 55. The Blogger's goal when selecting
-				the "Cheryl's Birthday Math Problem" to read is to find entertaining, educational, and informative articles that can be
-				included as a link from their blog post. Because they have unlimited time on their hands, they like to find
-				linkable pieces that will consume large amounts of other people's time as well. The Blogger's intent is basically
-				evil, but they too desire an easy to read page with fully functional links, and and easy method for both
-				re-posting and commenting. However, because they have a blog of their own, it is less likely they will post
-				a comment on the Huffington Post page unless they can include a link back to their WordPress site.<br />
-			<br />
-			The Blogger will be working from a tablet exclusively because couch surfing and bathing in the Starbuck's bathroom
-			necessitates a small digital footprint.
+			<p>The Blogger is a constant browser of media sites, looking for opportunities to write and information to
+				appropriate for their own content. The Blogger will have arrived at the same article as the Passionate
+				Reader/Commenter, but by selecting it from the HuffPost home page, not through a Twitter link.<br />
+				The Blogger group includes all genders and spans a large age range, but the numbers increase between 25 and 55
+				years old.<br />
+				The Blogger will be working from a desktop or a tablet because they work out of a home office and/or shared
+				workspace.
 			</p>
 		</blockquote>
 	</body>
--- specification/usecase.php
+++ specification/usecase.php
@@ -9,13 +9,13 @@
 		<h3>Passionate Reader/Commenter</h3>
 		<blockquote>
 			<ul>
-				<li>How? From a moble phone, a Male will click through from a Tweet sent from a follower. From a tablet, a Female will open the article while
-				browsing Huffington Post.</li>
-				<li>When? 24/7, while riding the Amtrak from NYC to DC.</li>
-				<li>Where? In a wifi enabled train car.</li>
-				<li>Purpose? Entertainment.</li>
-				<li>Scenarios? For the male, a friendly contest is posited by a Twitter follower. For the female, she is collecting challenges
-				for future use.</li>
+				<li>How? From a moble phone or tablet, a Passionate Reader/Commenter will click through from a Tweet sent
+					from a follower.</li>
+				<li>When? 24/7</li>
+				<li>Where? Anywhere a data connection is available.</li>
+				<li>Purpose? Entertainment and mental sparring.</li>
+				<li>Scenarios? Peer pressure to read and respond to the article, and then engage in a debate. This is the
+				discussion forum.</li>
 				<li>Necessary Steps?</li>
 					<ol>
 						<li>Receive Twitter with Huffpost link;</li>
@@ -28,19 +28,23 @@
 						<li>Facebook login screen opens for user id and password;</li>
 						<li>Click on Post Comment button;</li>
 						<li>Write comment;</li>
-						<li>Click "Post" button.</li>
+						<li>Click "Post" button;</li>
+						<li>Repeat.</li>
 					</ol>
 			</ul>
 		</blockquote>
 		<h3>Blogger</h3>
 		<blockquote>
 			<ul>
-				<li>How? From a tablet using wifi.</li>
+				<li>How? From a desktop or tablet via the home page of HuffPost.</li>
 				<li>When? 24/7</li>
-				<li>Where? In an all-night coffee shop with wifi.</li>
-				<li>Purpose? Collecting the writing of others to include in a post.</li>
-				<li>Scenarios? While browsing articles on the Huffington Post home page, will see
-					the "Cheryl's Birthday Math Challenge", and will click on the image to open http://www.huffingtonpost.com/2015/04/15/cheryls-birthday-answer_n_7067790.html</li>
+				<li>Where? Anywhere a data connection is available.</li>
+				<li>Purpose? Searching for writing opportunities and collecting the writing of others to include in a blog post.
+				The "discussion forum" is not HuffPost, but the Blogger's blog page.</li>
+				<li>Scenarios? While browsing articles on the Huffington Post home page, will see the "Cheryl's Birthday
+					Math Challenge" headline and will click on the image to open
+					http://www.huffingtonpost.com/2015/04/15/cheryls-birthday-answer_n_7067790.html, will scroll to read
+				and will email the article to their own address in order to include it in their next blog post.</li>
 				<li>Necessary Steps?</li>
 				<ol>
 					<li>Click on image from home page of Huffington Post;</li>
@@ -49,9 +53,7 @@
 					<li>Scroll down to Comments section to read posted comments;</li>
 					<li>Click "Like" and "Follow" on various comments;</li>
 					<li>Facebook login screen opens for user id and password;</li>
-					<li>Click on Post Comment button;</li>
-					<li>Click "Post" button.</li>
-
+					<li>Click on the Email icon to forward the article to their own address.</li>
 				</ol>
 			</ul>
 		</blockquote>
