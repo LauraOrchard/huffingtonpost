822e75be0198daf0bb3ed9cff7011cb4803cfc3e
Laura Orchard <artist@lauraorchard.com>
2015-04-16 11:17:54 -0600

    4/15/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.
                                                                                            Fleshing out the personas rather than using brief bullet points.
    
                                                                                            Same as above. Also doing research on actual demographics of math nerds.
    
                                                                                            Continuing with the details of the personas.
    
                                                                                            Hashing out the details of the html.
    
                                                                                            Added the second Persona of "Blogger" and completed the html check. Will review with Rochelle now.
    
                                                                                            Did not review with Rochelle yet, but began the Usecases file in html.
    
                                                                                            Completed Usecases for review with Dylan or Rochelle.
    
                                                                                            Tried to add the ERD as a Figure, but it failed.
    
                                                        4/16/15  Updated the usecase descriptions as per Rochelle suggestions.
    
                                                        Added the ERD svg image to images folder.
    
                                                        Moved ERD to the Specification folder
    
                                                        Refined ERD file name.
    
                                                        .gitignore
    
                                                        re-uploading the ERD
    
                                                        re-uploading the ERD
    
                                                        Created and added the schema description first pass.
    
                                                        Updated the ERD formatting.
    
    Trying to get the ERD.svg image to display on the web page.
    
    Trying to get the schema file to display on Git.
    
    Still trying to get the schema file to display on Git.
    
    Removed the blockquotes and duplicate title in the html on Personas.

--- specification/personas.php
+++ specification/personas.php
@@ -4,31 +4,26 @@
 		<meta charset="UTF-8">
 		<title>Data Design Project - April 14, 2015 - Personas</title>
 	</head>
-	<title>Huffington Post Article Personas</title>
 	<body>
 <h3>Persona #1: Passionate Reader/Commenter</h3>
-		<blockquote>
-			<p>The Passionate Reader/Commenter (PRC) is someone who has linked directly to a Huffington Post article because
-				of the subject matter. They wouldn't generally browse HuffPost, but when directed there to a specific article
-				by a group Tweet from a like-minded associate, they will read, re-post, re-Tweet, and comment with fervor. To
-				satisfy this type of reader, HuffPost needs to have all of the social media links and commenting functionality
-				working efficiently.<br />
-				The PRC group includes all genders and spans a large age range, but the numbers soar between 16 and 40 years old.<br />
-				The goals of the PRC group are to a) share interesting/informative articles and b) impress their opinions on others
-				through an interactive conversation in the comments section.<br />
-				The PRC group isn't browsing HuffPost, so their devices would be mobile phones and tablets.
-				</p>
-		</blockquote>
+		<p>The Passionate Reader/Commenter (PRC) is someone who has linked directly to a Huffington Post article because
+			of the subject matter. They wouldn't generally browse HuffPost, but when directed there to a specific article
+			by a group Tweet from a like-minded associate, they will read, re-post, re-Tweet, and comment with fervor. To
+			satisfy this type of reader, HuffPost needs to have all of the social media links and commenting functionality
+			working efficiently.<br />
+			The PRC group includes all genders and spans a large age range, but the numbers soar between 16 and 40 years old.<br />
+			The goals of the PRC group are to a) share interesting/informative articles and b) impress their opinions on others
+			through an interactive conversation in the comments section.<br />
+			The PRC group isn't browsing HuffPost, so their devices would be mobile phones and tablets.
+		</p>
 <h3>Persona #2: Blogger</h3>
-		<blockquote>
-			<p>The Blogger is a constant browser of media sites, looking for opportunities to write and information to
-				appropriate for their own content. The Blogger will have arrived at the same article as the Passionate
-				Reader/Commenter, but by selecting it from the HuffPost home page, not through a Twitter link.<br />
-				The Blogger group includes all genders and spans a large age range, but the numbers increase between 25 and 55
-				years old.<br />
-				The Blogger will be working from a desktop or a tablet because they work out of a home office and/or shared
-				workspace.
-			</p>
-		</blockquote>
+		<p>The Blogger is a constant browser of media sites, looking for opportunities to write and information to
+			appropriate for their own content. The Blogger will have arrived at the same article as the Passionate
+			Reader/Commenter, but by selecting it from the HuffPost home page, not through a Twitter link.<br />
+			The Blogger group includes all genders and spans a large age range, but the numbers increase between 25 and 55
+			years old.<br />
+			The Blogger will be working from a desktop or a tablet because they work out of a home office and/or shared
+			workspace.
+		</p>
 	</body>
 </html>
