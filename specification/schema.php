898eb9bd4b36cfe2a272c2e113d01e70816465ca
Laura Orchard <artist@lauraorchard.com>
2015-04-16 21:28:01 -0600

    4/15/15 - created the html for the text description of two personas for the Huffington Post article on a math problem.
                                                                                            Fleshing out the personas rather than using brief bullet points.
    
                                                                                            Same as above. Also doing research on actual demographics of math nerds.
    
                                                                                            Continuing with the details of the personas.
    
                                                                                            Hashing out the details of the html.
    
                                                                                            Added the second Persona of "Blogger" and completed the html check. Will review with Rochelle now.
    
                                                                                            Did not review with Rochelle yet, but began the Usecases file in html.
    
                                                                                            Completed Usecases for review with Dylan or Rochelle.
    
                                                                                            Tried to add the ERD as a Figure, but it failed.
    
                                                        4/16/15  Updated the usecase descriptions as per Rochelle suggestions.
    
                                                        Added the ERD svg image to images folder.
    
                                                        Moved ERD to the Specification folder
    
                                                        Refined ERD file name.
    
                                                        .gitignore
    
                                                        re-uploading the ERD
    
                                                        re-uploading the ERD
    
                                                        Created and added the schema description first pass.
    
                                                        Updated the ERD formatting.
    
    Trying to get the ERD.svg image to display on the web page.
    
    Trying to get the schema file to display on Git.
    
    Still trying to get the schema file to display on Git.
    
    Removed the blockquotes and duplicate title in the html on Personas.
    
    Made the edits to the schema and ERD after review with Dylan to reflect recursive loop on comments, no weak entities, and no counts.

--- specification/schema.php
+++ specification/schema.php
@@ -10,14 +10,14 @@
 			<li>Huffington Post does not require a login to read their articles, so the Reader is not an actual entity
 				for database purposes <em>until the Reader decides to "Like" or "Comment" on a page.</em></li>
 			<li>An Article is an Entity with 6 attributes (ArticleID[pk], ArticleTitle, SectionID, Content, PublishDate, AuthorID)</li>
-			<li>The Article is related to the Author via the ArticleID, an Entity with 3 attributes (AuthorID[pk], ArticleTitle, ArticleID)</li>
-			<li>If a Reader reads & responds to the Article, the FaceBook login form opens - this is not a database entity, but the FBuniqueID attribute
-				is captured for logging "Likes" and "Comments".</li>
-			<li>When the Reader "Likes" an article, a weak entity called Likes is created, with 5 attributes (LikeID[pk], ArticleID, LikeCount, FBuniqueID, LikeDate)</li>
-			<li>When the Reader "Comments" on at article, a weak entity called Comments is created, with 6 attributes (ParentCommentID[pk], ParentCommentContent, ParentCommentCount,
+			<li>The Article is related to the Author via the ArticleID, an Entity with 1 attribute (AuthorID[pk])</li>
+			<li>If a Reader reads & responds to the Article via the FaceBook Profile Entity, which has two attributes (FBuniqueID[pk], and Email) which are
+				captured for logging "Likes" and "Comments".</li>
+			<li>When the Reader "Likes" an article, an Entity called Likes is created, with 4 attributes (LikeID[pk], ArticleID, FBuniqueID, LikeDate)</li>
+			<li>When the Reader "Comments" on at article, an Entity called Comments is created, with 5 attributes (ParentCommentID[pk], ParentCommentContent,
 			ArticleID, ParentCommentDate, FBuniqueID)</li>
-			<li>When the Reader makes a comment on a comment, a weak entity called ChildComment is created, with 6 attributes (ChildCommentID[pk], ChildCommentContent, FBuniqueID, ChildCommentCount, ChildCommentDate, ParentCommentID) </li>
-			<li>When the Reader "Likes" a comment, a weak entity called CommentLikes is created, with 5 attributes (CommentLikeID[pk], ParentCommentID, CommentLikeCount,
+			<li>When the Reader makes a comment on a comment, a reiterative loop is goes back to Comment.</li>
+			<li>When the Reader "Likes" a comment, an Entity called CommentLikes is created, with 4 attributes (CommentLikeID[pk], ParentCommentID,
 			FBuniquID, CommentLikeDate)</li>
 			<li>The reason the FBuniqueID is included in several spots is because a new Reader can choose to "Like a Comment" or "Comment on
 				a Comment" as their first interaction with the article.</li>
