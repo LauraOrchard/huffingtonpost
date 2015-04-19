0b6241c0e393d7cddd9ebc1d6b6af72123e51d4c
Laura Orchard <artist@lauraorchard.com>
2015-04-19 14:24:54 -0600

    Modified the ERD and the schema text to match the database tables.
    Changed the ERD.svg to .pdf to see if it will fix the display problem.
    
    Testing the CSS stylesheet.
    
    Added formatting to the ERD, and continue to have issues with the ERD.svg image link within the document.

--- specification/personas.php
+++ specification/personas.php
@@ -7,24 +7,29 @@
 	</head>
 	<body>
 <h3>Persona #1: Passionate Reader/Commenter</h3>
-		<p>The Passionate Reader/Commenter (PRC) is someone who has linked directly to a Huffington Post article because
-			of the subject matter. They wouldn't generally browse HuffPost, but when directed there to a specific article
-			by a group Tweet from a like-minded associate, they will read, re-post, re-Tweet, and comment with fervor. To
-			satisfy this type of reader, HuffPost needs to have all of the social media links and commenting functionality
-			working efficiently.<br />
-			The PRC group includes all genders and spans a large age range, but the numbers soar between 16 and 40 years old.<br />
-			The goals of the PRC group are to a) share interesting/informative articles and b) impress their opinions on others
-			through an interactive conversation in the comments section.<br />
-			The PRC group isn't browsing HuffPost, so their devices would be mobile phones and tablets.
-		</p>
+		<p>
+			<ul>
+				<li>The Passionate Reader/Commenter (PRC) is someone who has linked directly to a Huffington Post article because
+				of the subject matter. They wouldn't generally browse HuffPost, but when directed there to a specific articleby a group Tweet from a like-minded associate, they will read, re-post, re-Tweet, and comment with fervor. To
+				satisfy this type of reader, HuffPost needs to have all of the social media links and commenting functionality
+				working efficiently.</li>
+				<li>The PRC group includes all genders and spans a large age range, but the numbers soar between 16 and 40 years old.</li>
+				<li>The goals of the PRC group are to a) share interesting/informative articles and b) impress their opinions on others
+				through an interactive conversation in the comments section.</li>
+				<li>The PRC group isn't browsing HuffPost, so their devices would be mobile phones and tablets.</li>
+			</ul>
 <h3>Persona #2: Blogger</h3>
-		<p>The Blogger is a constant browser of media sites, looking for opportunities to write and information to
-			appropriate for their own content. The Blogger will have arrived at the same article as the Passionate
-			Reader/Commenter, but by selecting it from the HuffPost home page, not through a Twitter link.<br />
-			The Blogger group includes all genders and spans a large age range, but the numbers increase between 25 and 55
-			years old.<br />
-			The Blogger will be working from a desktop or a tablet because they work out of a home office and/or shared
-			workspace.
+		<p>
+			<ul>
+				<li>The Blogger is a constant browser of media sites, looking for opportunities to write and information to
+				appropriate for their own content. The Blogger will have arrived at the same article as the Passionate
+				Reader/Commenter, but by selecting it from the HuffPost home page, not through a Twitter link.</li>
+				<li>The Blogger group includes all genders and spans a large age range, but the numbers increase between 25 and 55
+				years old.</li>
+				<li>The goal of the Blogger is to keep churning around information, even if it isn't original, well written, or even true.</li>
+				<li>The Blogger will be working from a desktop or a tablet because they work out of a home office and/or shared
+				workspace.</li>
+			</ul>
 		</p>
 	</body>
 </html>
