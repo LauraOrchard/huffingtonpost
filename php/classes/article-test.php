9e7cda467c9d6fc2c5771f2bf8c53cadbdaf2dbe
Laura Orchard <artist@lauraorchard.com>
2015-04-23 16:52:11 -0600

    Modified the ERD and the schema text to match the database tables.
                                                                                                             Changed the ERD.svg to .pdf to see if it will fix the display problem.
    
                                                                                                             Testing the CSS stylesheet.
    
                                                                                                             Added formatting to the ERD, and continue to have issues with the ERD.svg image link within the document.
    
                                                                                                             Removed the CSS stylesheet on the personas.php document.
    
                                                                                                             Dylan and Rochelle waved their magic wands to correct the display. Used browse remote host from PHPStorm.
                                                                                                             Created the code for the Article class.
                                                                                                             Completed and fixed all code in the Article class.
                                                                                                             Also created the Article-Test for encryption.
                                                                                                             Readying for debugging step.

--- php/classes/article-test.php
+++ php/classes/article-test.php
@@ -3,7 +3,6 @@
 require_once("/etc/apache2/data-design/encrypted-config.php");
 
 
-
 // use the constructor to create a new Article
 require_once("article.php");
 $article = new Article(1, "Everything", "Four score and seven years ago...", "12-23-2014");
