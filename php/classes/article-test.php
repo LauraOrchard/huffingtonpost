c0d2572b27810bdbddb4c6e5cd34604e1e57d04b
Laura Orchard <artist@lauraorchard.com>
2015-04-23 16:55:32 -0600

    Corrected date

--- php/classes/article-test.php
+++ php/classes/article-test.php
@@ -5,6 +5,6 @@ require_once("/etc/apache2/data-design/encrypted-config.php");
 
 // use the constructor to create a new Article
 require_once("article.php");
-$article = new Article(1, "Everything", "Four score and seven years ago...", "12-23-2014");
+$article = new Article(1, "Everything", "Four score and seven years ago...", "2015-12-23 12:22:24");
 $config=readConfig("/etc/apache2/data-design/lorchard.ini");
 ?>
