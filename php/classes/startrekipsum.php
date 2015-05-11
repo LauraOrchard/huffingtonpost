cdf75df8ba49aa70e883b7a5c30563d88f153f8d
Laura Orchard <artist@lauraorchard.com>
2015-05-11 14:24:53 -0600

    Star Trek exercise.

--- php/classes/startrekipsum.php
+++ php/classes/startrekipsum.php
@@ -2,9 +2,36 @@
 <html>
 	<head lang="en">
 		<meta charset="UTF-8">
-		<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
-		<script src="startrek.js" type="application/javascript">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- ////////////////////////////////////////////////
+		 //// LINK TO YOUR CUSTOM CSS FILES HERE
+		 //// <link type="text/css" href="css/style.css" rel="stylesheet"/>
+		 ///////////////////////////////////////////////////// -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<script src="startrek.js" type="application/javascript"></script>
 			<title>Star Trek Ipsum</title>
 	</head>
 	<body>
@@ -20,7 +47,7 @@
 		<p>It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex,
 		the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would
 		produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I
-		haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase
+		haven not determined if our phaser energy can generate a stable field. We could alter the photons with phase
 		discriminators.
 		</p>
 	</body>
