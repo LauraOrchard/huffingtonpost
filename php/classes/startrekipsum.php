d6345144e6bf8c1099d5d15ee240ad23cde6c8b3
Laura Orchard <artist@lauraorchard.com>
2015-05-11 14:13:28 -0600

    Star Trek exercise.

--- php/classes/startrekipsum.php
+++ php/classes/startrekipsum.php
@@ -2,16 +2,21 @@
 <html>
 	<head lang="en">
 		<meta charset="UTF-8">
-		<title>Star Trek Ipsum</title>
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
+		<script src="startrek.js" type="application/javascript">
+
+			<title>Star Trek Ipsum</title>
 	</head>
 	<body>
-		<a id="popUp" class="btn btn-primary" onclick="popUp();">HOT!</a>
-		<div id="earlGrey" style="display: none;">
-			<h3>Earl Grey...</h3>
-			<p>HOT!.</p>
-			<div id="closePopUp" onclick="closeEarlGrey();">X</div>
+		<a id="popUp" class="btn btn-primary" onclick="popUp();">Earl Grey...</a>
+		<br>
+		<br>
+		<div id="hotTea" style="display: none;">
+			<h3>Refreshment</h3>
+			<p>HOT!!</p>
+			<div id="closePopUp" onclick="closeHotTea();">X</div>
 		</div>
-		<p>Jean Luc would press the button above.</p>
+		<p>Jean Luc would click on the button above.</p>
 		<p>It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex,
 		the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would
 		produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I
