7c263f64c63a0eb95ebc2c30c2cbb00cb39c82c4
Laura Orchard <artist@lauraorchard.com>
2015-05-19 22:35:23 -0600

    Special assignment

---
+++ who-me/justme.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<title>Just Little 'ol me</title>
+		<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript">
+			$( document ).ready(function() {
+				console.log( "je suis prest!" );
+				$("body").append("<p>bon jour</p>");
+				for(var i=0; i < 10; i++)
+				{
+					$.ajax({
+						type: "POST",
+						url: "https://bootcamp-coders.cnm.edu/classmaterials/week3/mvc/controller-post.php",
+						data: {
+							profileId: "1",
+							tweetContent: "Je ne suis pas un criminel!"
+						},
+						success: function(){
+							console.log("With a little help from my friend...");
+						}
+// dataType: dataType
+					});
+				}
+
+			});
+			$
+
+
+		</script>
+	</head>
+	<body>
+
+	</body>
+</html>
