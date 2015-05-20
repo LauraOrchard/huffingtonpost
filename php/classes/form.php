7ae22c19b56aff0927ae6d06b0ed90a846ae21fa
Laura Orchard <artist@lauraorchard.com>
2015-05-19 21:15:43 -0600

    Special assignment

--- php/classes/form.php
+++ php/classes/form.php
@@ -10,9 +10,8 @@
 				Name: <input type="text" name="name"><br>
 				E-mail: <input type="text" name="email"><br>
 				<input type="submit">
-			</form>
 
-		<?php echo $_POST["name"]; ?><br>
-		<?php echo $_POST["email"]; ?>
+			</form>
 	</body>
 </html>
+
